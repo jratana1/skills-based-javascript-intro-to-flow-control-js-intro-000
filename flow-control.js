05a2f74b5d1283ffa2dc53bdf298a8044e873f1f
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-25 17:30:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,25 +26,25 @@ function ternaryTeenager(age) {if (13<=age && age < 19)
 function switchAge(age) {switch(age){
   case "13":
       return ("You are a teenager")
-  break;
+      ;
   case "14":
       return ("You are a teenager")
-  break;
+      ;
   case "15":
       return ("You are a teenager")
-  break;
+      ;
   case "16":
       return ("You are a teenager")
-  break;
+      ;
   case "17":
       return ("You are a teenager")
-  break;
+      ;
   case "18":
       return ("You are a teenager")
-  break;
+      ;
   case "19":
       return ("You are a teenager")
-  break;
+      ;
   default : return ("You have an age")
  }
 
