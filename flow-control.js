bf802ed7e32586e0418cd99489ad30f004c68d82
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-25 16:58:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,4 @@
-function basicTeenager(age) {if (13 < age && age < 19)
+function basicTeenager(age) {if (13 <= age && age <= 19)
         return ("You are a teenager!")
         else ("You are not a teenager!")
 
